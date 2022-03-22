b78d7724bf745ffaabc9b345cefbef583f8e2aa4
ernesto_araujo <ernestopcaraujo@gmail.com>
2022-03-22 08:31:02 -0300

    Exercício finalizado e testado

--- semana04Exercicios/semana04Ex01.js
+++ semana04Exercicios/semana04Ex01.js
@@ -1,8 +1,24 @@
-var teste=1;
-var ops=2;
-function yeah(teste,ops){
-    console.log(teste);
-    console.log(ops);
+/*
+Utilizando estrutura condicional exiba (console.log) a descrição 
+da classificação da idade (variável) da pessoa de acordo com os dados abaixo:
+
+Jovens - Indivíduos de até 15 anos;
+Adultos - Indivíduos com idade entre 16 até 64 anos;
+Idosos - Indivíduos de 65 anos em diante.
+*/
+//
+var idade = 65;
+var faixaEtaria;
+var jovem = 15;
+var adulto = 65;
+
+if (idade <= jovem) {
+    faixaEtaria = "Jovem"
 }
-console.log(teste);
-console.log(ops);
+if (idade > jovem && idade < adulto){
+    faixaEtaria = "Adulto"
+}
+if (idade >= adulto){
+    faixaEtaria = "3a Idade"
+}
+console.log(faixaEtaria);
