863ecee6fce711c930a6d1d05ea823490e07d6a8
ernesto_araujo <ernestopcaraujo@gmail.com>
2022-03-22 08:34:37 -0300

    Reenviando o arquivo

--- semana04Exercicios/semana04Ex01.js
+++ semana04Exercicios/semana04Ex01.js
@@ -6,8 +6,10 @@ Jovens - Indivíduos de até 15 anos;
 Adultos - Indivíduos com idade entre 16 até 64 anos;
 Idosos - Indivíduos de 65 anos em diante.
 */
-//
-var idade = 65;
+//foi colocado um window.prompt para que o teste pudesse ser feito com
+//diversos valores.
+//os resultados são exibidos via console.log. 
+var idade = parseInt(window.prompt("Digite sua idade abaixo:"));
 var faixaEtaria;
 var jovem = 15;
 var adulto = 65;
