852151bb4e1869a7bc9475b602a9711cbea76d5f
Ernesto Araújo <101370745+ernestopcaraujo@users.noreply.github.com>
2022-06-20 00:51:37 -0300

    GeraEstoque V1 - Finalização do ProdutoRepository.cs

--- semana15Exercicios/GeraEstoque/Repositories/ProdutoRepository.cs
+++ semana15Exercicios/GeraEstoque/Repositories/ProdutoRepository.cs
@@ -1,7 +1,14 @@
 namespace GeraEstoque.Repositories;
 using GeraEstoque.Models;
 
-public class ProdutoRespository {
+public class ProdutoRepository {
+
+    public IList<Produto> Produtos { get; set; }
+
+  public ProdutoRepository()
+  {
+    Produtos = new List<Produto>();
+  }
 
 
 }
